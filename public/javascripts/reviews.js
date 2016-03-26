c32180fd11e84643f247f4b870a4c3b153c54433
Tyler Bainbridge <tyler_bainbridge@student.uml.edu>
2016-03-26 02:58:55 -0400

    updated

--- public/javascripts/reviews.js
+++ public/javascripts/reviews.js
@@ -3,7 +3,7 @@
  */
 var reviewMarkup = "";
 for(var i = 0; i<album.reviews.length; i++){
-    reviewMarkup += "<p>" + album.reviews[i].writtenReview + "</p><br>" + "<p>"+ album.reviews[i].score +"</p>";
+    reviewMarkup += "<p>" + album.reviews[i].writtenReview + "</p><br>" + "<p>"+ album.reviews[i].score +"/10</p>";
 }
 
 $("#userreviews").html(reviewMarkup);
